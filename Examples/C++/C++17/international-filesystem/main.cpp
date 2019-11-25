e9cdb469e8dd228992bca7c30b6e661890dc1abc
aaron.wilson <aaron.wi@gmail.com>
2019-11-25 22:59:58 +0000

    added compilation guards around windows specific code

--- Examples/C++/C++17/international-filesystem/main.cpp
+++ Examples/C++/C++17/international-filesystem/main.cpp
@@ -24,7 +24,10 @@ int main( int argc, char** argv )
 {
     try
     {
+#ifdef WIN32
         { // Create a file using a utf16 encoded wstring and write utf8 data to it
+
+            // This only works on windows because by filesystem and iostream work in utf8, not utf16 
             std::filesystem::path path(utf8_to_utf16("울란바토르.txt"));
             std::ofstream f(path);
             if(!f.is_open())
@@ -32,6 +35,7 @@ int main( int argc, char** argv )
 
             f << "𠜎𠜱𠝹𠱓𠱸𠲖𠳏";
         }
+#endif
 
         { // Create a file using a utf8 encoded string and write utf8 data to it
             std::filesystem::path path = std::filesystem::u8path("猫.txt");
@@ -41,6 +45,19 @@ int main( int argc, char** argv )
 
             f << "찦차를 타고 온";
         }
+
+#ifndef WIN32
+        { // Create a file (same as above) only this time the u8path type is not required since we are on Linux where utf8 is the norm
+            std::filesystem::path path("猫猫.txt");
+            std::ofstream f(path);
+            if(!f.is_open())
+                throw std::runtime_error("unable to open file");
+
+            f << "찦차를 타고 온온온온온";
+
+            // While this works on Linux, it is better to specify the path using u8path since that is cross platform
+        }
+#endif
 
 #ifdef WIN32
         { // Get a utf16 encoded wstring from the Windows API, convert it to utf8, append a utf8 filename and then write to the file
